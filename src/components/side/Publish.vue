59a99ade3b3aebb7e76b85635c2207d0044131e7
SuperGuangDi <547579053@qq.com>
2019-07-03 00:36:12 +0800

    save

--- src/components/side/Publish.vue
+++ src/components/side/Publish.vue
@@ -1,23 +1,30 @@
 <template>
   <div class="publish-cnt">
-    <div class="box upload-box">
+    <div class="box">
       <div class="title">上传音乐</div>
+      <div class="upload-box">
+        <input type="file" style="display: none" ref="file" @change="getFile" />
+        <el-button @click="selectFile" plain>选择文件</el-button>
+        <el-button
+          plain
+          @click="dialogVisible=true"
+          size="mini"
+          icon="el-icon-view"
+          circle
+          v-show="showHash"
+        ></el-button>
+        <span>&emsp;{{fileName}}</span>
+      </div>
     </div>
     <div class="box music-box">
       <div class="title">登记信息</div>
-      <div class="value-box">
-        <div class="value-line">
-          <div class="value-item">
-            <span>作品名称：</span>
-            <el-input class="myinput" v-model="form.music_name"></el-input>
-          </div>
-          <div class="value-item">
-            <span>作品种类：</span>
-            <el-input class="myinput" v-model="form.music_kind"></el-input>
-          </div>
+      <div class="value-line">
+        <div class="value-item">
+          <div class="label">作品名称：</div>
+          <el-input class="myinput" v-model="form.music_name"></el-input>
         </div>
         <div class="value-item">
-          <span>录制时间：</span>
+          <div class="label">录制时间：</div>
           <el-date-picker
             style="width: 300px"
             v-model="form.record_time"
@@ -25,34 +32,70 @@
             placeholder="选择日期"
           ></el-date-picker>
         </div>
-        <div class="value-item">
-          <span>首句歌词：</span>
-          <el-input class="myinput" v-model="form.lyric" style="width: 600px"></el-input>
-        </div>
       </div>
     </div>
     <div class="box author-box">
       <div class="title">作者信息</div>
+      <div class="value-item">
+        <div class="label">作者：</div>
+        <el-input class="myinput" v-model="form.author"></el-input>
+      </div>
     </div>
     <div class="box owner-box">
       <div class="title">版权人信息</div>
+      <div class="value-line">
+        <div class="value-item">
+          <div class="label">姓名：</div>
+          <el-input class="myinput" v-model="form.owner_name"></el-input>
+        </div>
+        <div class="value-item">
+          <div class="label">{{isCompany ? '企业编号': '身份证号码'}}：</div>
+          <el-input class="myinput" v-model="form.owner_id"></el-input>
+        </div>
+      </div>
+      <div class="value-item">
+        <div class="label">所在地：</div>
+        <el-input class="myinput" v-model="form.owner_addr"></el-input>
+      </div>
+      <div class="value-line">
+        <div class="value-item">
+          <div class="label">联系电话：</div>
+          <el-input class="myinput" v-model="form.owner_phone"></el-input>
+        </div>
+        <div class="value-item">
+          <div class="label">电子邮箱：</div>
+          <el-input class="myinput" v-model="form.owner_email"></el-input>
+        </div>
+      </div>
     </div>
-    <div class="btn-box"></div>
+    <div class="btn-box">
+      <el-button @click="submitMusic" plain>确认登记</el-button>
+    </div>
+    <el-dialog title="文件的SHA512值" :visible.sync="dialogVisible" width="30%">
+      <span>{{fileHash}}</span>
+      <span slot="footer" class="dialog-footer">
+        <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
+      </span>
+    </el-dialog>
   </div>
 </template>
 
 <script>
+var SHA512 = require('crypto-js/sha512')
+var Base64 = require('crypto-js/enc-base64.js')
+
 export default {
   data() {
     return {
+      fileName: '请选择文件',
+      fileHash: '',
+      showHash: false,
+      dialogVisible: false,
       form: {
         music_name: '',
-        music_kind: '',
+        publish_time: '',
         record_time: '',
-        lyric: '',
-        author_ci: '',
-        author_qu: '',
-        author_si: '',
+        author: '',
         owner_name: '',
         owner_id: '',
         owner_addr: '',
@@ -60,26 +103,58 @@ export default {
         owner_email: ''
       }
     }
+  },
+  computed: {
+    isCompany() {
+      return this.$store.state.userType === 'company'
+    }
+  },
+  methods: {
+    selectFile() {
+      this.$refs.file.click()
+    },
+    getFile(e) {
+      let fileObj = e.target.files[0]
+      this.fileName = fileObj.name
+      var reader = new FileReader()
+      reader.readAsDataURL(fileObj)
+      this.showHash = true
+      reader.onload = () => {
+        var str = reader.result.split(',')[1]
+        var urlDecoded = decodeURIComponent(str)
+        var base64Decoded = Base64.parse(urlDecoded)
+        var hashed = SHA512(base64Decoded).toString()
+        this.fileHash = hashed
+      }
+    },
+    submitMusic() {
+      // to do
+      var date = new Date()
+      var datestr =
+        date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate()
+      this.form.publish_time = datestr
+      console.log('submit', this.form.publish_time)
+    }
   }
 }
 </script>
 
 <style lang="stylus" scoped>
 .publish-cnt {
-  width: 950px;
+  width: 920px;
   border: 1px solid #e9e9e9;
   box-shadow: 0 2px 8px 0 rgba(0, 0, 0, 0.1);
   border-radius: 8px;
   margin: 40px auto;
-  padding: 10px 20px;
+  padding: 10px 36px;
 
   .box {
-    margin: 10px 0;
+    margin: 20px 0;
 
     .title {
       width: 200px;
-      color: #a9a9a9;
-      border-bottom: 1px solid #a9a9a9;
+      color: #bababa;
+      border-bottom: 1px solid #bababa;
     }
 
     .value-line {
@@ -91,6 +166,30 @@ export default {
       font-size: 1.125rem;
       width: 300px;
     }
+
+    .value-item {
+      display: flex;
+      align-items: center;
+      margin-top: 10px;
+      margin-bottom: 15px;
+
+      .label {
+        width: 100px;
+      }
+    }
+  }
+
+  .upload-box {
+    margin-top: 10px;
+
+    span {
+      color: #606266;
+    }
+  }
+
+  .btn-box {
+    text-align: center;
+    margin-bottom: 20px;
   }
 }
 </style>
